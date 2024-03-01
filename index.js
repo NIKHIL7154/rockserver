00f282beecf601f9a485a39cc1baf6a922340647
NIKHIL <nikhilthakurq@gmail.com>
2024-03-01 11:17:18 +0530

    origin updated

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/checkrooms',(req,res)=>{
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://rockpapersci.vercel.app/",
+        origin: "https://rockpapersci.vercel.app",
         methods: ["POST", "GET"],
     },
 
